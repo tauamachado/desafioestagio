c8f0c61275c077d080c3592046ac2e2f2dbeefc7
Tauã Machado <taua.machado@estudante.ufjf.br>
2023-09-16 18:19:09 -0300

    Finalizando cabeçalho e rodapé

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,51 @@
 <template>
+  <div>
     <div id="nav">
-        <router-link to="/">
-            <img src="" id="logo">
-        </router-link>
-        <router-link to="/">Home</router-link> |
-        <router-link to="/pedidos">Pedidos</router-link>
+      <router-link id="logo-url" to="/">
+        <img id="logo" :src="logo" :alt="alt">
+      </router-link>
+      <router-link to="/">Fazer Pedido</router-link>
+      <router-link to="/pedidos">Pedidos</router-link>
     </div>
+  </div>
 </template>
+
 <script>
+
 export default {
-    name: "Navbar"
+  name: "Navbar",
+  props: ["logo", "alt"]
 }
 </script>
-<style scoped>
 
+<style scoped>
+  #nav {
+    background-color: #222;
+    border-bottom: 4px solid #111;
+    padding: 15px 50px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
+
+  #nav #logo-url {
+    margin: auto;
+    margin-left: 0;
+  }
+
+  #logo {
+    width: 40px;
+    height: 40px;
+  }
+
+  #nav a {
+    color: #fcba03;
+    text-decoration: none;
+    margin: 12px;
+    transition: .5s;
+  }
+
+  #nav a:hover {
+    color: #FFF;
+  }
 </style>

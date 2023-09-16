4355c451bf76df4c6663e7e8981eeb067d01554b
Tauã Machado <taua.machado@estudante.ufjf.br>
2023-09-16 20:57:13 -0300

    Criando formulário de cadastro

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,22 @@
 <template>
-  <Banner />
-  <div class="main-container">
-    <h1>Monte o seu burger:</h1>
-    <p>formulário</p>
+  <div>
+    <Banner />
+    <div class="main-container">
+      <h1>Monte o seu burger:</h1>
+      <BurgerForm />
+    </div>
   </div>
 </template>
 
 <script>
-import Banner from "../components/Banner.vue"
+import Banner from '../components/Banner'
+import BurgerForm from '../components/BurgerForm'
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
-    Banner
+    Banner,
+    BurgerForm
   }
 }
 </script>

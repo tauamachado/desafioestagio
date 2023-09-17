c1d996b756ed038e29d63bc748965c338aa7d5ba
Tauã Machado <taua.machado@estudante.ufjf.br>
2023-09-16 21:13:21 -0300

    Resgatando dados do banco e inserindo no formulário

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -35,7 +35,74 @@
 </template>
 
 <script>
+export default {
+  name: "BurgerForm",
+  data() {
+    return {
+      paes: null,
+      carnes: null,
+      opcionaisdata: null,
+      nome: null,
+      pao: null,
+      carne: null,
+      opcionais: [],
+      status: "Solicitado",
+      msg: null
+    }
+  },
+  methods: {
+    async getIngredientes() {
+      const req = await fetch('http://localhost:3000/ingredientes')
+      const data = await req.json()
 
+      this.paes = data.paes
+      this.carnes = data.carnes
+      this.opcionaisdata = data.opcionais
+    },
+    async createBurger(e) {
+
+      e.preventDefault()
+
+      const data = {
+        nome: this.nome,
+        carne: this.carne,
+        pao: this.pao,
+        opcionais: Array.from(this.opcionais),
+        status: "Solicitado"
+      }
+
+      const dataJson = JSON.stringify(data)    
+
+      const req = await fetch("http://localhost:3000/burgers", {
+        method: "POST",
+        headers: { "Content-Type" : "application/json" },
+        body: dataJson
+      });
+
+      const res = await req.json()
+
+      console.log(res)
+
+      this.msg = "Pedido realizado com sucesso!"
+
+      // clear message
+      setTimeout(() => this.msg = "", 3000)
+
+      // limpar campos
+      this.nome = ""
+      this.carne = ""
+      this.pao = ""
+      this.opcionais = []
+      
+    }
+  },
+  mounted () {
+    this.getIngredientes()
+  },
+  components: {
+    
+  }
+}
 </script>
 
 <style scoped>
